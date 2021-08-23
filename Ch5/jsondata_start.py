8272df91d664a2994b20c1a3f0cb36e4afc059a0
Robiul Hossen <Robiul.Hossen@yahoo.com>
2021-08-24 01:49:27 +1000

    Adding code upto json read

--- Ch5/jsondata_start.py
+++ Ch5/jsondata_start.py
@@ -2,25 +2,52 @@
 # Example file for parsing and processing JSON
 #
 import urllib.request 
+import json
 
 def printResults(data):
   # Use the json module to load the string data into a dictionary
   theJSON = json.loads(data)
+
   
   # now we can access the contents of the JSON like any other Python object
 
-  
+  # if "title" in theJSON["metadata"]:
+  #     print(theJSON["metadata"]["title"])
+  #     print(theJSON["metadata"]["api"])
   # output the number of events, plus the magnitude and each event name  
+  # if "count" in theJSON["metadata"]:
+  #     print("Total number of eventthe:" +str(theJSON["metadata"]["count"]))
+  # for i in theJSON["features"]:
+  #      print("Place : " +str(i["properties"]["place"]))
+  #      print("Event : " +str(i["properties"]["type"]))
+  #      print("The magnatitude of the event : " +str(i["properties"]["mag"]))
+  #      print("--------------\n")
 
   
-  # for each event, print the place where it occurred
+    # for each event, print the place where it occurred
 
 
   # print the events that only have a magnitude greater than 4
-
+  # for i in theJSON["features"]:
+  #   mag = (i["properties"]["mag"])
+  #   if mag >= 5:
+  #      print("Place : " +str(i["properties"]["place"]))
+  #      print("Event : " +str(i["properties"]["type"]))
+  #      print("The magnatitude of the event : " +str(i["properties"]["mag"]))
+  #      print("--------------\n")
 
   # print only the events where at least 1 person reported feeling something
 
+  for i in theJSON["features"]:
+    feltsomething = (i["properties"]["felt"])
+    if feltsomething != None:
+      if feltsomething != "null":
+        if feltsomething > 0:
+            print("Place : " +str(i["properties"]["place"]))
+            print("Event : " +str(i["properties"]["type"]))
+            print("The magnatitude of the event : " +str(i["properties"]["mag"]))
+            print("Felt something : " +str(i["properties"]["felt"]))
+            print("--------------\n")
   
 def main():
   # define a variable to hold the source URL
@@ -32,6 +59,14 @@ def main():
   webUrl = urllib.request.urlopen(urlData)
   print ("result code: " + str(webUrl.getcode()))
   
+  if (webUrl.getcode() == 200):
+    data = webUrl.read().decode("utf-8")
+    printResults(data)
+
+  else:
+        print("Received an error from server, cannot retrieve results " +
+              str(webUrl.getcode()))
+
 
 if __name__ == "__main__":
   main()
